7b50e25839cd62892886da1adb72ce356e173906
Aaron Caldwell <86275216+acaldwell6890@users.noreply.github.com>
2022-03-07 22:13:13 -0500

    Update server.js
    API Calls on  server

--- server.js
+++ server.js
@@ -8,3 +8,16 @@ const port = 3000
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+app.use('/', express.static(path.join(__dirname, '../client')))
+
+
+app.get('/', async (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'));
+})
+
+app.get('/', async (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/protectpage.html'));
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
